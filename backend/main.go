77b36dc5e1d47b81875ec8ed88d633337a4b4163
sokq44 <139003906+sokq44@users.noreply.github.com>
2024-10-07 19:22:43 +0200

    1 set up (#2)
    * chore: add a showcase of a basic connection between backend and frontend
    
    * chore: add tailwindcss to the frontend

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var PORT uint16 = 3000
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", HelloWorld)
+
+	log.Printf("Serving on http://localhost:%d", PORT)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil); err != nil {
+		log.Fatal("Error while trying to start the server.")
+	}
+
 }

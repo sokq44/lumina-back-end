40b2e15c41adc798ccc6cc8b3a3641ec9578b474
sokq44 <139003906+sokq44@users.noreply.github.com>
2024-10-09 20:44:09 +0200

    3 setup database (#9)
    * order: minor changes in the file structure and README.md files
    
    * chore: add manual for setting up the database & create the users table structure
    
    * chore: update manual and main readme

---
+++ backend/db/db-snapshot.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS articles;
+
+USE articles;
+
+CREATE TABLE
+    users (
+        id VARCHAR(36) PRIMARY KEY NOT NULL DEFAULT (uuid ()),
+        name VARCHAR(50) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(64) NOT NULL
+    )

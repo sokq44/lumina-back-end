1a1567577abc8b54da0f787d5ca85e6ce3ade958
sokq44 <sokq44@gmail.com>
2024-11-28 20:52:15 +0100

    chore: type in the CORS middleware

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -13,7 +13,7 @@ import (
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH. PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {

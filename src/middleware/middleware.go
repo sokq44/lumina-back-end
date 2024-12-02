82b3f200741532d1b1ced0c4130b73bf1e62a28f
sokq44 <sokq44@gmail.com>
2024-12-02 20:00:03 +0100

    fix: typo in the Access-Control-Allow-Methods header

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -16,7 +16,7 @@ func CORS(next http.HandlerFunc) http.HandlerFunc {
 		if origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH. PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 

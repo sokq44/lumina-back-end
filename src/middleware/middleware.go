9a0d5c8c36c68357e471c5bdd01bbd0276647f6f
sokq44 <sokq44@gmail.com>
2024-12-02 19:40:18 +0100

    chore: Access-Cotrol-Allow-Credentials changed to true in the CORS middleware

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -15,6 +15,7 @@ func CORS(next http.HandlerFunc) http.HandlerFunc {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH. PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
